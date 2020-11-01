f66c581b71f189c5e1fa0237eee690d4de0c08fb
Limongi <alex.limongi@epitech.eu>
2020-11-01 19:34:57 +0100

    finish

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import screen_search
 import cv2
 import random
@@ -8,9 +9,8 @@ import sys
 import asyncio
 import threading
 
-class Coffre_twitch(threading.Thread):
+class Coffre_twitch():
     def __init__(self):
-        threading.Thread.__init__(self)
         self.imagePath = "coffre_twitch.png"
         self.searchClass = screen_search.Search(self.imagePath)
         self.position = list
@@ -19,7 +19,15 @@ class Coffre_twitch(threading.Thread):
         self.howManyDontFind = int = 0
         self.img = cv2.imread(self.imagePath)
         self.heightOfImage, self.widthOfImage, self.channelsOfImage = self.img.shape
-        self.breakWhile = False
+        self.breakWhile = True
+
+    def _asyncio_thread(self, async_loop):
+        # async_loop.run_forever(self.run())
+        async_loop.run_until_complete(self.run())
+
+    def _asyncio_thread_stop(self, async_loop):
+        async_loop.stop()
+        async_loop.close()
 
     def setHowManyYouWantToFind(self, howManyYouWantToFind_):
         try:
@@ -42,7 +50,15 @@ class Coffre_twitch(threading.Thread):
         pyautogui.click(button="left")
 
     async def run(self):
-        while (self.countFind != self.howManyYouWantToFind or self.breakWhile != True):
+        self.howManyDontFind = 0
+        self.countFind = 0
+        while (self.breakWhile != True):
+            # print(self.countFind, self.howManyYouWantToFind)
+            if  (self.countFind == self.howManyYouWantToFind):
+                return
+            timeToSleep = random.randint(60, 75 + self.countFind)
+            timeRemeber = 0
+            # print(self.breakWhile)
             if (self.searchImage() == True):
                 self.countFind = self.countFind + 1
                 print("Image found at position : {} {}\t countFind = {} / try left = {}"
@@ -51,14 +67,22 @@ class Coffre_twitch(threading.Thread):
                 # await asyncio.sleep(random.randint(60, 75 + self.countFind))
                 # await time.sleep(random.randint(60, 75 + self.countFind))
                 # asyncio.sleep(random.randint(60, 75 + self.countFind))
-                time.sleep(random.randint(60, 75 + self.countFind))
+                # time.sleep(random.randint(60, 75 + self.countFind))
             else:
                 self.howManyDontFind = self.howManyDontFind + 1
                 print("Image not found, howManyDontFind = {}".format(self.howManyDontFind))
                 # await asyncio.sleep(random.randint(30, 45 + self.countFind))
                 # await time.sleep(random.randint(30, 45 + self.countFind))
                 # asyncio.sleep(random.randint(30, 45 + self.countFind))
-                time.sleep(random.randint(30, 45 + self.countFind))
+                # time.sleep(random.randint(30, 45 + self.countFind))
+
+            while (timeToSleep != timeRemeber):
+                if (self.breakWhile == True):
+                    return
+                timeRemeber = timeRemeber + 1
+                time.sleep(1)
+                # print("one sec pass")
+        return
 
 class Application(tkinter.Frame):
     def __init__(self, master=None):
@@ -68,11 +92,20 @@ class Application(tkinter.Frame):
         self.master = master
         self.pack()
         self.create_widgets()
-        
+        self.async_loop = asyncio.get_event_loop()
+        self.clock()
+        self.myTask = None
 
     def create_widgets(self):
         # self.labelHello = tkinter.Label(self, text="Auto Click Coffre", font=("Arial Bold", 30))
         # self.labelHello.grid(column=1, row=0)
+        countFindTextStatus_var = tkinter.StringVar()
+        countFindTextStatus_var.set(str(self.coffre_twitch.countFind))
+        howManyDontFindStatus_var = tkinter.StringVar()
+        howManyDontFindStatus_var.set(str(self.coffre_twitch.howManyDontFind))
+        tryLeft_var = tkinter.StringVar()
+        tryLeft_var.set(str(self.coffre_twitch.howManyYouWantToFind - self.coffre_twitch.countFind))
+        # self.master.update_idletasks()
 
         self.labelText = tkinter.Label(self, text="Number : ", font=("Arial Bold", 15))
         self.labelText.grid(sticky=tkinter.W)
@@ -92,17 +125,17 @@ class Application(tkinter.Frame):
 
         self.countFindText = tkinter.Label(self, text="countFind : ", font=("Arial Bold", 15))
         self.countFindText.grid(column=0, row=3, sticky=tkinter.W)
-        self.countFindTextStatus = tkinter.Label(self, text=self.coffre_twitch.countFind, font=("Arial Bold", 15))
+        self.countFindTextStatus = tkinter.Label(self, textvariable=countFindTextStatus_var, font=("Arial Bold", 15))
         self.countFindTextStatus.grid(column=1, row=3, sticky=tkinter.E)
 
         self.howManyDontFindText = tkinter.Label(self, text="howManyDontFind : ", font=("Arial Bold", 15))
         self.howManyDontFindText.grid(column=0, row=4, sticky=tkinter.W)
-        self.howManyDontFindStatusText = tkinter.Label(self, text=self.coffre_twitch.howManyDontFind, font=("Arial Bold", 15))
+        self.howManyDontFindStatusText = tkinter.Label(self, textvariable=howManyDontFindStatus_var, font=("Arial Bold", 15))
         self.howManyDontFindStatusText.grid(column=1, row=4, sticky=tkinter.E)
 
         self.tryLeftText = tkinter.Label(self, text="Try left : ", font=("Arial Bold", 15))
         self.tryLeftText.grid(column=0, row=5, sticky=tkinter.W)
-        self.tryLeftTextStatus = tkinter.Label(self, text=(self.coffre_twitch.howManyYouWantToFind - self.coffre_twitch.countFind), font=("Arial Bold", 15))
+        self.tryLeftTextStatus = tkinter.Label(self, textvariable=tryLeft_var, font=("Arial Bold", 15))
         self.tryLeftTextStatus.grid(column=1, row=5, sticky=tkinter.E)
 
         self.errorText = tkinter.Label(self, font=("Arial Bold", 15), fg="red")
@@ -117,22 +150,64 @@ class Application(tkinter.Frame):
         #                       command=self.master.destroy)
         # self.quit.pack(side="bottom")
 
+    def clock(self):
+        countFindTextStatus_var = tkinter.StringVar()
+        countFindTextStatus_var.set(str(self.coffre_twitch.countFind))
+        howManyDontFindStatus_var = tkinter.StringVar()
+        howManyDontFindStatus_var.set(str(self.coffre_twitch.howManyDontFind))
+        tryLeft_var = tkinter.StringVar()
+        tryLeft_var.set(str(self.coffre_twitch.howManyYouWantToFind - self.coffre_twitch.countFind))
+
+        self.countFindTextStatus.config(textvariable=countFindTextStatus_var)
+        self.howManyDontFindStatusText.config(textvariable=howManyDontFindStatus_var)
+        self.tryLeftTextStatus.config(textvariable=tryLeft_var)
+        self.master.after(1000, self.clock)
+
+    # def startFun(self):
+    #     if (self.coffre_twitch.setHowManyYouWantToFind(self.entryText.get()) == True):
+    #         self.errorText["text"] = ""
+    #         self.actualStatusText["text"] = "Work"
+    #         print("succefully set")
+    #         asyncio.run(self.coffre_twitch.run())
+    #         # self.coffre_twitch.main()
+    #     else:
+    #         self.errorText["text"] = "Enter int"
+    #         print("Fail set")
+    #     # print(self.howManyYouWantToFind.get())
+
     def startFun(self):
+        if (self.coffre_twitch.breakWhile == False):
+            self.errorText["text"] = "Already in work"
+            print("Already in work")
+            return
+        
         if (self.coffre_twitch.setHowManyYouWantToFind(self.entryText.get()) == True):
             self.errorText["text"] = ""
             self.actualStatusText["text"] = "Work"
+            self.coffre_twitch.breakWhile = False
             print("succefully set")
-            asyncio.run(self.coffre_twitch.run())
+            self.myTask = threading.Thread(target=self.coffre_twitch._asyncio_thread, args=(self.async_loop,))
+            self.myTask.start()
+            # asyncio.run(self.coffre_twitch.run())
             # self.coffre_twitch.main()
+            return
         else:
             self.errorText["text"] = "Enter int"
             print("Fail set")
-        # print(self.howManyYouWantToFind.get())
+            return
 
     def endFun(self):
         self.errorText["text"] = ""
         self.actualStatusText["text"] = "Stop"
+        # self.myTask._stop()
+        # self.myTask.daemon = False
+        # self.myTask._delete()
         self.coffre_twitch.breakWhile = True
+        # self.coffre_twitch._asyncio_thread_stop(self.async_loop)
+        # self.myTask.join()
+        # del self.myTask
+        # sys.exit()
+        # raise KeyboardInterrupt
 
 if __name__ == "__main__":
     # if (len(sys.argv) == 2):
@@ -143,3 +218,4 @@ if __name__ == "__main__":
     root = tkinter.Tk()
     app = Application(master=root)
     app.mainloop()
+    # app.update_idletasks()
